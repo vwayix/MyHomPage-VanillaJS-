98df55c86700253439c3581fb69412e5ba22d5cc
vwayix <vwayix@gmail.com>
2024-10-19 23:26:54 +0900

    channel html에 반복되는 내용 js로 수정
    myInfor부분만 일단은 완벽

--- js/app.js
+++ js/app.js
@@ -1,25 +1,58 @@
-const headerbtn = document.querySelector("#headerbtn");
-const skill = document.querySelector("#skill");
-const project = document.querySelector("#project");
-const signIn = document.querySelector("#signIn");
-const signUp = document.querySelector("#signUp");
-const mainTitle = document.querySelector(".notion h1");
-const title = document.querySelector("#title");
-const text = document.querySelector(".notion p");
+function displayClassNames() {
+    // coworkerList 클래스를 가진 모든 ul 요소를 선택
+    var coworkerLists = document.querySelectorAll(".coworkerList");
 
-headerbtn.addEventListener("onclick", function(){
-    skill.innerHTML = "skill";
-    project.innerHTML = "project";
-    signIn.innerHTML = "signIn";
-    signUp.innerHTML = "signUp";
-    mainTitle.innerHTML = "welcome";
-    title.innerHTML = "This is a space to share experiences and various projects accumulated through web development.";
+    // 각 ul 요소에 대해 반복문 실행
+    coworkerLists.forEach(function (ul) {
+        // 클래스 이름을 <p>로 출력
 
-    text.innerHTML = 
-    "This website is a space where I create various projects using what I learned while studying web development, and record and share the results."+ 
-    "At first, we started by creating simple web pages, but in the future, we plan to gradually develop by challenging app development and projects using various APIs."
-    "I am improving my skills and learning new skills by creating my own web and carrying out various projects."
-    "This site is a portfolio of completed projects and experiences along the way."
-    "Additionally, I am running this site to provide some help to those who want to learn web development like me."
-    "In the future, I plan to record my growth as a web developer through more practice and projects here. thank you!";
-})
+
+        var classItem = document.createElement("p");
+        classItem.textContent = "클래스 이름: " + ul.className; // 클래스 이름 설정
+        document.body.appendChild(classItem); // body에 <p> 요소 추가
+    });
+}
+
+const myInfor = document.querySelector(".my-infor");
+const skillInfor = document.querySelector(".skill-infor");
+
+const myTitle = ['About Me', 'CHANNEL', '자격증'];
+let myDeta = [['2006.05.19'], ['부산컴퓨터과학고등학교졸업예정'], ['깃허브'], ['컴퓨터활용능력2급']];
+const skillTitle = ['FrontEnd', 'Framework', 'BackEnd', 'DB', 'Server', 'Tools', '프로그래밍언어들', 'Collaboration Design & Document'];
+let skillData = [['HTML, CSS, JAVASCRIPT'], ['Node.js, React.js, Next.js'], ['미공개'], ['MongoDB, MySQL'],
+['Apache, Apache tomcat'], ['VSCODE, 이클립스, DEV C++'], ['C언어, JAVA'], ['노션, 깃허브, 피그마, 파워포인트, 엑셀, 한글']];
+
+//my
+let myContent = '';
+for(let i = 0; i < 3; i++){
+    if (i < 1) { 
+        myContent += '<div><h3>' + myTitle[i] + '</h3><p>' + myDeta[i] + '</p><br><p>' + myDeta[i+1] + '</p></div>';
+    } else {
+        myContent += '<div><h3>' + myTitle[i] + '</h3><p>' + myDeta[i+1] + '</p></div>';
+    }
+}
+myInfor.innerHTML = myContent; 
+
+//skill
+let skillContent = '';
+for(let j = 0; j < 8; j++){
+    skillContent += '<div><h3>' + skillTitle[j] + '</h3><p>' + skillData[j] + '</p></div>';
+}
+skillInfor.innerHTML = skillContent;
+
+/* 
+<h1>Loop & Array</h1>
+<script>
+    var coworkers = ['egoing', 'leezche', 'duru', 'taeho'];
+</script>
+<h2>Co workers</h2>
+<ul>
+    <script>
+        var i = 0;
+        while(i < coworkers.length){
+            document.write('<li><a href="http://a.com/'+coworkers[i]+'">'+coworkers[i]+'</a></li>');
+            i = i + 1;
+        }
+    </script>
+</ul>
+*/
