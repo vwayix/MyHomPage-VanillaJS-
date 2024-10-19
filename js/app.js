078da5bb5bbf3f2d73ee5c858b8baad1a3b72835
vwayix <vwayix@gmail.com>
2024-10-20 02:40:20 +0900

    channel html -> js 수정
    myinfor, skillinfor 완벽히 수정완료

--- js/app.js
+++ js/app.js
@@ -2,29 +2,53 @@ const myInfor = document.querySelector(".my-infor");
 const skillInfor = document.querySelector(".skill-infor");
 
 const myTitle = ['About Me', 'CHANNEL', '자격증'];
-let myDeta = [['2006.05.19'], ['부산컴퓨터과학고등학교졸업예정'], ['깃허브'], ['컴퓨터활용능력2급']];
-const skillTitle = ['FrontEnd', 'Framework', 'BackEnd', 'DB', 'Server', 'Tools', '프로그래밍언어들', 'Collaboration Design & Document'];
-let skillData = [['HTML, CSS, JAVASCRIPT'], ['Node.js, React.js, Next.js'], ['미공개'], ['MongoDB, MySQL'],
-['Apache, Apache tomcat'], ['VSCODE, 이클립스, DEV C++'], ['C언어, JAVA'], ['노션, 깃허브, 피그마, 파워포인트, 엑셀, 한글']];
+let myData = [['2006.05.19'], ['부산컴퓨터과학고등학교졸업예정'], ['깃허브'], ['컴퓨터활용능력2급']];
 
-//my
+const skillTitle = {
+    flex1: ['FrontEnd', 'Framework'],
+    flex2: ['BackEnd', 'DB', 'Server'],
+    flex3: ['Tools', '프로그래밍언어들', 'Collaboration Design & Document']
+};
+let skillData = {
+    flex1: ['HTML, CSS, JAVASCRIPT', 'Node.js, React.js, Next.js'],
+    flex2: ['미공개', 'MongoDB, MySQL', 'Apache, Apache tomcat'],
+    flex3: ['VSCODE, 이클립스, DEV C++', 'C언어, JAVA', '노션, 깃허브, 피그마, 파워포인트, 엑셀, 한글']
+};
+
+//myInfor
 let myContent = '';
-for(let i = 0; i < 3; i++){
-    if (i < 1) { 
-        myContent += '<div><h3>' + myTitle[i] + '</h3><p>' + myDeta[i] + '</p><br><p>' + myDeta[i+1] + '</p></div>';
+for (let i = 0; i < 3; i++) {
+    if (i < 1) {
+        myContent += '<div><h3>' + myTitle[i] + '</h3><p>' + myData[i] + '</p><br><p>' + myData[i + 1] + '</p></div>';
     } else {
-        myContent += '<div><h3>' + myTitle[i] + '</h3><p>' + myDeta[i+1] + '</p></div>';
+        myContent += '<div><h3>' + myTitle[i] + '</h3><p>' + myData[i + 1] + '</p></div>';
     }
 }
-myInfor.innerHTML = myContent; 
+myInfor.innerHTML = myContent;
 
-//skill
-let skillContent = '';
-for(let j = 0; j < 8; j++){
-    skillContent += '<div><h3>' + skillTitle[j] + '</h3><p>' + skillData[j] + '</p></div>';
+//skillInfor
+for (var i = 1; i <= 3; i++) {
+    var newDiv = document.createElement('div');
+    newDiv.className = 'flex' + i;
+    var skillContent = '';
+    switch (newDiv.className) {
+        case 'flex1':
+            for (j = 0; j < 2; j++) {
+                skillContent += '<div><h3>' + skillTitle['flex' + i][j] + '</h3><p>' + skillData['flex' + i][j] + '</p></div>';
+            }
+            newDiv.innerHTML = skillContent;
+            skillInfor.appendChild(newDiv);
+            break;
+        case 'flex2':
+        case 'flex3':
+            for (j = 0; j < 3; j++) {
+                skillContent += '<div><h3>' + skillTitle['flex' + i][j] + '</h3><p>' + skillData['flex' + i][j] + '</p></div>';
+            }
+            newDiv.innerHTML = skillContent;
+            skillInfor.appendChild(newDiv);
+        default:
+    }
 }
-skillInfor.innerHTML = skillContent;
-
 /* 
 <h1>Loop & Array</h1>
 <script>
